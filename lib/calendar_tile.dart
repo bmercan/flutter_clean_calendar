8d372cf49b93584fd6c61dda3759a8d51c49c610
bmercan <bayram.178@gmail.com>
2022-10-23 01:54:39 +0300

    test

--- lib/calendar_tile.dart
+++ lib/calendar_tile.dart
@@ -123,7 +123,9 @@ class CalendarTile extends StatelessWidget {
                     ? Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: events!.map((event) {
-                          if (!event.isDone) eventCount++;
+                          if (!event.isDone) {
+                            eventCount++;
+                          }
                           // Show a maximum of 3 dots.
                           if (eventCount > 3) return SizedBox();
                           return Container(
